1404ffc8ff2d9415d499318af587e6c77c3518c8
matsufriends <pikapikan0526@gmail.com>
2024-10-18 12:35:11 +0900

    MornUGUIGlobalの追加

---
+++ MornUGUIGlobal.cs
@@ -0,0 +1,22 @@
+﻿using MornGlobal;
+using UnityEngine;
+
+namespace MornUGUI
+{
+    [CreateAssetMenu(fileName = nameof(MornUGUIGlobal), menuName = "Morn/" + nameof(MornUGUIGlobal))]
+    internal sealed class MornUGUIGlobal : MornGlobalBase<MornUGUIGlobal>
+    {
+#if DISABLE_MORN_ASPECT_LOG
+        protected override bool ShowLog => false;
+#else
+        protected override bool ShowLog => true;
+#endif
+        protected override string ModuleName => nameof(MornUGUI);
+        [SerializeField] private AudioClip _buttonCursorClip;
+        [SerializeField] private AudioClip _buttonSubmitClip;
+        [SerializeField] private AudioClip _buttonCancelClip;
+        internal AudioClip ButtonCursorClip => _buttonCursorClip;
+        internal AudioClip ButtonSubmitClip => _buttonSubmitClip;
+        internal AudioClip ButtonCancelClip => _buttonCancelClip;
+    }
+}
