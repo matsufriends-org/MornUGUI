f26a256d13443afe02d2b0d900912633948083ca
matsufriends <pikapikan0526@gmail.com>
2025-02-02 19:05:30 +0900

    ColorにInteractable分岐追加

--- Button/MornUGUIButtonColorModule.cs
+++ Button/MornUGUIButtonColorModule.cs
@@ -8,9 +8,13 @@ namespace MornUGUI
     internal sealed class MornUGUIButtonColorModule : MornUGUIButtonModuleBase
     {
         [SerializeField] private Image _image;
+        [Header("interactable")]
         [SerializeField] private Color _focusedColor = Color.white;
         [SerializeField] private Color _unfocusedColor = Color.gray;
-
+        [Header("not interactable")]
+        [SerializeField] private Color _focusedColor2 = Color.white;
+        [SerializeField] private Color _unfocusedColor2 = Color.gray;
+        
         public override void Awake(MornUGUIButton parent)
         {
             if (_image == null)
@@ -18,7 +22,7 @@ namespace MornUGUI
                 return;
             }
 
-            _image.color = _unfocusedColor;
+            _image.color = parent.IsInteractable ? _unfocusedColor : _unfocusedColor2;
         }
 
         public override void OnSelect(MornUGUIButton parent)
@@ -28,7 +32,7 @@ namespace MornUGUI
                 return;
             }
 
-            _image.color = _focusedColor;
+            _image.color = parent.IsInteractable ? _focusedColor : _focusedColor2;
         }
 
         public override void OnDeselect(MornUGUIButton parent)
@@ -38,7 +42,7 @@ namespace MornUGUI
                 return;
             }
 
-            _image.color = _unfocusedColor;
+            _image.color = parent.IsInteractable ? _unfocusedColor : _unfocusedColor2;
         }
     }
 }
