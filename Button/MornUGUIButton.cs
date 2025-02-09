4f97cb91a33879f4bb55fe6a3285948676cefd2b
matsufriends <pikapikan0526@gmail.com>
2025-02-09 19:42:56 +0900

    IsInteractableをMornUGUIButton独自のものに

--- Button/MornUGUIButton.cs
+++ Button/MornUGUIButton.cs
@@ -26,11 +26,7 @@ namespace MornUGUI
         [SerializeField] private MornUGUIButtonConvertPointerToSelectModule _convertPointerToSelectModule;
         [SerializeField] private MornUGUIButtonSoundModule _soundModule;
         [Inject] private IMornFlagGetter _flagGetter;
-        public bool IsInteractable
-        {
-            get => _button.interactable;
-            set => _button.interactable = value;
-        }
+        public bool IsInteractable { get; set; }
         public bool IsNegative => _isNegative;
         public IMornFlagGetter FlagGetter => _flagGetter;
         public IObservable<Unit>　OnButtonSelected => _button.OnSelectAsObservable().Select(_ => Unit.Default);
